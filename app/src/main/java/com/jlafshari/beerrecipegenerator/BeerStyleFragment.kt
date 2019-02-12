c01ec736c046e2a969ae1a400e6c7533559ff2e1
Jahan <jlafshari@yahoo.com>
2019-02-11 18:09:14 -0600

    Added spinner to select beer style.

--- app/src/main/java/com/jlafshari/beerrecipegenerator/BeerStyleFragment.kt
+++ app/src/main/java/com/jlafshari/beerrecipegenerator/BeerStyleFragment.kt
@@ -5,10 +5,20 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class BeerStyleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_beer_style, container, false)
+        val view = inflater.inflate(R.layout.fragment_beer_style, container, false)
+
+        val adapter = ArrayAdapter(context, R.layout.support_simple_spinner_dropdown_item,
+            listOf("American Brown Ale", "English Special Bitter", "IPA"))
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+        val styleSpinner = view.findViewById<Spinner>(R.id.styleSpinner)
+        styleSpinner.adapter = adapter
+
+        return view
     }
 }
