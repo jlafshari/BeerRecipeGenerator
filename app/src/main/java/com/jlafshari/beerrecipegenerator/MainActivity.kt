e0de82e431b39c93b4bce6e3312ebd065d732314
Jahan Afshari <afsharij@wwt.com>
2020-08-07 13:50:04 -0700

    upgrade android support to androidx, update Kotlin version

--- app/src/main/java/com/jlafshari/beerrecipegenerator/MainActivity.kt
+++ app/src/main/java/com/jlafshari/beerrecipegenerator/MainActivity.kt
@@ -2,9 +2,9 @@ package com.jlafshari.beerrecipegenerator
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -19,7 +19,12 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val recipeListView = findViewById<RecyclerView>(R.id.recipeRecyclerView)
-        recipeListView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+        recipeListView.layoutManager =
+            LinearLayoutManager(
+                this,
+                LinearLayout.VERTICAL,
+                false
+            )
         val recipes = ArrayList<RecipePreview>()
         recipes.add(RecipePreview("FSB"))
         recipes.add(RecipePreview("Jahan's Private Reserve"))
--- app/src/main/java/com/jlafshari/beerrecipegenerator/NewRecipeWizardActivity.kt
+++ app/src/main/java/com/jlafshari/beerrecipegenerator/NewRecipeWizardActivity.kt
@@ -1,11 +1,11 @@
 package com.jlafshari.beerrecipegenerator
 
 import android.os.Bundle
-import android.support.design.widget.TabLayout
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentPagerAdapter
-import android.support.v7.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.jlafshari.beerrecipecore.RecipeGenerationInfo
