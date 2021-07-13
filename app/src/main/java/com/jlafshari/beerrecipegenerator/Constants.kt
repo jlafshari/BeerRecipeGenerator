a331f82e989b5a49330327958918300b8cad69f3
Jahan Afshari <jahan.afshari@wwt.com>
2021-07-12 17:38:05 -0700

    add constant for default boil duration in minutes

--- app/src/main/java/com/jlafshari/beerrecipegenerator/Constants.kt
+++ app/src/main/java/com/jlafshari/beerrecipegenerator/Constants.kt
@@ -8,4 +8,5 @@ object Constants {
     const val EXTRA_VIEW_RECIPE = "ViewRecipe"
     const val EXTRA_SIGN_OUT = "SignOut"
     const val PREF_STORAGE_WEB: String = "web_client"
+    const val BOIL_DURATION_TIME_DEFAULT = 60
 }
