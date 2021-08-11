4b6e0874f2f3f2a411df9dbebfdb40d4d3dde237
Jahan Afshari <jahan.afshari@wwt.com>
2021-08-11 12:17:04 -0700

    when viewing abv picker page, reset the values when if the user has picked a different beer style; also retain the abv value if the user has previously picked one

--- app/src/main/java/com/jlafshari/beerrecipegenerator/newRecipe/AbvFragment.kt
+++ app/src/main/java/com/jlafshari/beerrecipegenerator/newRecipe/AbvFragment.kt
@@ -14,33 +14,52 @@ import com.jlafshari.beerrecipegenerator.R
 
 class AbvFragment : Fragment() {
     private lateinit var mCallback: AbvCallback
+    private lateinit var mAbvValues: List<Double>
+    private lateinit var mRecipeAbvPicker: NumberPicker
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_abv, container, false)
+        mRecipeAbvPicker = view.findViewById(R.id.abvPicker)
 
-        val abvValues = getAbvValues(mCallback.getAbvThreshold())
-        val startingAbvIndex = getMedianAbvIndex(abvValues)
-        val recipeAbvPicker = view.findViewById<NumberPicker>(R.id.abvPicker)
-        setUpRecipeAbvPicker(recipeAbvPicker, abvValues.map { it.toString() }, startingAbvIndex)
+        mAbvValues = getAbvValues(mCallback.getAbvThreshold())
+        val startingAbvIndex = setPickerToMedianValue()
 
-        mCallback.onAbvValueSet(abvValues[startingAbvIndex])
+        mCallback.onAbvValueSet(mAbvValues[startingAbvIndex])
 
         return view
     }
 
+    override fun onResume() {
+        mAbvValues = getAbvValues(mCallback.getAbvThreshold())
+        val recipeGenerationInfo = mCallback.getCurrentRecipeGenerationInfo()
+        if (recipeGenerationInfo.abv != null) {
+            val index = mAbvValues.indexOf(recipeGenerationInfo.abv)
+            setUpRecipeAbvPicker(mAbvValues.map { it.toString() }, index)
+        } else {
+            setPickerToMedianValue()
+        }
+        super.onResume()
+    }
+
+    private fun setPickerToMedianValue(): Int {
+        val startingAbvIndex = getMedianAbvIndex(mAbvValues)
+        setUpRecipeAbvPicker(mAbvValues.map { it.toString() }, startingAbvIndex)
+        return startingAbvIndex
+    }
+
     private fun setUpRecipeAbvPicker(
-        recipeAbvPicker: NumberPicker,
         abvValues: List<String>,
         startingAbvIndex: Int
     ) {
-        recipeAbvPicker.minValue = 0
-        recipeAbvPicker.maxValue = abvValues.size - 1
-        recipeAbvPicker.value = startingAbvIndex
-        recipeAbvPicker.displayedValues = abvValues.toTypedArray()
-        recipeAbvPicker.wrapSelectorWheel = false
+        mRecipeAbvPicker.displayedValues = null
+        mRecipeAbvPicker.minValue = 0
+        mRecipeAbvPicker.maxValue = abvValues.size - 1
+        mRecipeAbvPicker.value = startingAbvIndex
+        mRecipeAbvPicker.displayedValues = abvValues.toTypedArray()
+        mRecipeAbvPicker.wrapSelectorWheel = false
 
-        recipeAbvPicker.setOnValueChangedListener { _, _, newVal ->
+        mRecipeAbvPicker.setOnValueChangedListener { _, _, newVal ->
             mCallback.onAbvValueSet(abvValues[newVal].toDoubleOrNull())
         }
     }
@@ -54,7 +73,7 @@ class AbvFragment : Fragment() {
         }
     }
 
-    interface AbvCallback {
+    interface AbvCallback : RecipeInfoListener {
         fun onAbvValueSet(abv: Double?)
         fun getAbvThreshold(): StyleThreshold
     }
