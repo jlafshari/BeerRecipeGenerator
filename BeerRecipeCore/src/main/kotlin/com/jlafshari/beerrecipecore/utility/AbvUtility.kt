512482d9759cce356b365bec02e3f944d60bb5ca
Jahan Afshari <jahan.afshari@wwt.com>
2021-08-25 14:46:32 -0700

    move abv values list of function to a property in the containing class

--- BeerRecipeCore/src/main/kotlin/com/jlafshari/beerrecipecore/utility/AbvUtility.kt
+++ BeerRecipeCore/src/main/kotlin/com/jlafshari/beerrecipecore/utility/AbvUtility.kt
@@ -3,11 +3,10 @@ package com.jlafshari.beerrecipecore.utility
 import com.jlafshari.beerrecipecore.StyleThreshold
 
 object AbvUtility {
-    fun getAbvValues(abvThreshold: StyleThreshold): List<Double> {
-        val potentialAbvValues = listOf(3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0)
+    private val potentialAbvValues = listOf(3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0)
 
-        return potentialAbvValues.filter { it in abvThreshold.minimum..abvThreshold.maximum }
-    }
+    fun getAbvValues(abvThreshold: StyleThreshold): List<Double> =
+        potentialAbvValues.filter { it in abvThreshold.minimum..abvThreshold.maximum }
 
     fun getMedianAbvIndex(abvValues: List<Double>): Int = (abvValues.size - 1) / 2
 }
