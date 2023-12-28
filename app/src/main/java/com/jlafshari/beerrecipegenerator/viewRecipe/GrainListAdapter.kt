7a5f2072af1e6229c7e10cd412673a9d1629cf2d
Jahan Afshari <jahan.afshari@wwt.com>
2023-12-27 16:38:21 -0800

    display color of fermentable

--- app/src/main/java/com/jlafshari/beerrecipegenerator/viewRecipe/GrainListAdapter.kt
+++ app/src/main/java/com/jlafshari/beerrecipegenerator/viewRecipe/GrainListAdapter.kt
@@ -5,15 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.jlafshari.beerrecipecore.FermentableIngredient
 import com.jlafshari.beerrecipegenerator.R
+import com.jlafshari.beerrecipegenerator.srmColors.Colors
 
 class GrainListAdapter(private val grainList: List<FermentableIngredient>, private val context: Context) :
     RecyclerView.Adapter<GrainListAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val grain = grainList[position]
+        val color = Colors.getColor(grain.color.toInt())
+        if (color != null)
+            holder.srmGrainColorCardView.setCardBackgroundColor(color.rbgColor)
         holder.txtGrain.text = context.getString(R.string.grainListItem, grain.amount.toString(), grain.name)
     }
 
@@ -28,5 +33,6 @@ class GrainListAdapter(private val grainList: List<FermentableIngredient>, priva
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val txtGrain: TextView = itemView.findViewById(R.id.txtGrain)
+        val srmGrainColorCardView: CardView = itemView.findViewById(R.id.srmGrainColorCardView)
     }
 }
