a03649f5e31ce71dda053931071de121bbac32ba
Jahan Afshari <jahan.afshari@wwt.com>
2024-01-09 16:32:31 -0800

    display recipe abv and color on main activity

--- BeerRecipeCore/src/main/kotlin/com/jlafshari/beerrecipecore/recipes/RecipePreview.kt
+++ BeerRecipeCore/src/main/kotlin/com/jlafshari/beerrecipecore/recipes/RecipePreview.kt
@@ -2,6 +2,6 @@ package com.jlafshari.beerrecipecore.recipes
 
 data class RecipePreview(val id: String, val name: String,
                          val type: RecipeType, val isVersioned: Boolean,
-                         val versionNumber: Int) {
+                         val versionNumber: Int, val abv: Float, val colorSrm: Int) {
     override fun toString(): String = name
 }
--- app/src/main/java/com/jlafshari/beerrecipegenerator/recipes/RecipeListAdapter.kt
+++ app/src/main/java/com/jlafshari/beerrecipegenerator/recipes/RecipeListAdapter.kt
@@ -1,21 +1,33 @@
 package com.jlafshari.beerrecipegenerator.recipes
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.jlafshari.beerrecipecore.recipes.RecipePreview
 import com.jlafshari.beerrecipegenerator.R
+import com.jlafshari.beerrecipegenerator.srmColors.Colors
 
 class RecipeListAdapter(private val recipeList: List<RecipePreview>,
                         private val clickListener: (RecipePreview) -> Unit) :
     RecyclerView.Adapter<RecipeListAdapter.ViewHolder>() {
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val recipePreview = recipeList[position]
-        holder.txtName.text = recipePreview.toString()
-        holder.bind(recipePreview, clickListener)
+        with (holder) {
+            txtName.text = recipePreview.toString()
+            txtAbv.text = recipePreview.abv.toString() + "%"
+
+            val color = Colors.getColor(recipePreview.colorSrm)
+            if (color != null)
+                srmColorCardView.setCardBackgroundColor(color.rbgColor)
+
+            bind(recipePreview, clickListener)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,6 +39,8 @@ class RecipeListAdapter(private val recipeList: List<RecipePreview>,
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val txtName: TextView = itemView.findViewById(R.id.txtName)
+        val txtAbv: TextView = itemView.findViewById(R.id.txtAbv)
+        val srmColorCardView: CardView = itemView.findViewById(R.id.srmColorCardView)
 
         fun bind(recipePreview: RecipePreview, clickListener: (RecipePreview) -> Unit) =
             itemView.setOnClickListener { clickListener(recipePreview) }
