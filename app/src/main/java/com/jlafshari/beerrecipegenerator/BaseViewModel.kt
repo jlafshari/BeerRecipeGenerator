f4445ecdd626ed475d05bcb2baf4f59564d0bf89
Jahan Afshari <jahan.afshari@wwt.com>
2024-03-01 11:37:29 -0800

    refactor code to dispose retrofit calls into a base view model class

---
+++ app/src/main/java/com/jlafshari/beerrecipegenerator/BaseViewModel.kt
@@ -0,0 +1,19 @@
+package com.jlafshari.beerrecipegenerator
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+abstract class BaseViewModel : ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+
+    internal fun Disposable.disposeWhenCleared(): Disposable {
+        compositeDisposable.add(this)
+        return this
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
+}
--- app/src/main/java/com/jlafshari/beerrecipegenerator/recipes/RecipeViewModel.kt
+++ app/src/main/java/com/jlafshari/beerrecipegenerator/recipes/RecipeViewModel.kt
@@ -4,19 +4,17 @@ import android.annotation.SuppressLint
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.jlafshari.beerrecipecore.recipes.RecipePreview
+import com.jlafshari.beerrecipegenerator.BaseViewModel
 import com.jlafshari.beerrecipegenerator.HomebrewApiService
 import com.jlafshari.beerrecipegenerator.ui.login.AzureAuthHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 @HiltViewModel
-class RecipeViewModel @Inject constructor(private val homebrewApiService: HomebrewApiService) : ViewModel() {
-    private val compositeDisposable = CompositeDisposable()
+class RecipeViewModel @Inject constructor(private val homebrewApiService: HomebrewApiService) : BaseViewModel() {
 
     private val _loadRecipePreviewsResponse = MutableLiveData<List<RecipePreview>>()
     val loadRecipePreviewsResponse: LiveData<List<RecipePreview>> = _loadRecipePreviewsResponse
@@ -28,22 +26,17 @@ class RecipeViewModel @Inject constructor(private val homebrewApiService: Homebr
     fun loadRecipePreviews(abvMin: String?, abvMax: String?,
                            colorMin: String?, colorMax: String?,
                            yeastType: String?) {
-        val disposable = homebrewApiService.getAllRecipePreviews(loadAccessTokenResponse.value, abvMin, abvMax, colorMin, colorMax, yeastType)
+        homebrewApiService.getAllRecipePreviews(loadAccessTokenResponse.value, abvMin, abvMax, colorMin, colorMax, yeastType)
             .subscribeOn(Schedulers.newThread())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 { _loadRecipePreviewsResponse.postValue(it) },
                 { Log.d("", "load recipe previews error ", it) }
             )
-        compositeDisposable.add(disposable)
+            .disposeWhenCleared()
     }
 
     fun loadAccessToken() {
         AzureAuthHelper.getAccessTokenAsync { _loadAccessTokenResponse.postValue(it) }
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        compositeDisposable.clear()
     }
 }
