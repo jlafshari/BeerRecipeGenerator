f7f046dc8c7ffe5c12b9bd5e0c2ce31ff1128334
Jahan Afshari <jahan.afshari@wwt.com>
2024-03-01 07:58:27 -0800

    dispose of retrofit call

--- app/src/main/java/com/jlafshari/beerrecipegenerator/recipes/RecipeViewModel.kt
+++ app/src/main/java/com/jlafshari/beerrecipegenerator/recipes/RecipeViewModel.kt
@@ -10,13 +10,17 @@ import com.jlafshari.beerrecipegenerator.HomebrewApiService
 import com.jlafshari.beerrecipegenerator.ui.login.AzureAuthHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 @HiltViewModel
 class RecipeViewModel @Inject constructor(private val homebrewApiService: HomebrewApiService) : ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+
     private val _loadRecipePreviewsResponse = MutableLiveData<List<RecipePreview>>()
     val loadRecipePreviewsResponse: LiveData<List<RecipePreview>> = _loadRecipePreviewsResponse
+
     private val _loadAccessTokenResponse = MutableLiveData<String?>()
     val loadAccessTokenResponse: LiveData<String?> = _loadAccessTokenResponse
 
@@ -24,16 +28,22 @@ class RecipeViewModel @Inject constructor(private val homebrewApiService: Homebr
     fun loadRecipePreviews(abvMin: String?, abvMax: String?,
                            colorMin: String?, colorMax: String?,
                            yeastType: String?) {
-        homebrewApiService.getAllRecipePreviews(loadAccessTokenResponse.value, abvMin, abvMax, colorMin, colorMax, yeastType)
+        val disposable = homebrewApiService.getAllRecipePreviews(loadAccessTokenResponse.value, abvMin, abvMax, colorMin, colorMax, yeastType)
             .subscribeOn(Schedulers.newThread())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 { _loadRecipePreviewsResponse.postValue(it) },
                 { Log.d("", "load recipe previews error ", it) }
             )
+        compositeDisposable.add(disposable)
     }
 
     fun loadAccessToken() {
         AzureAuthHelper.getAccessTokenAsync { _loadAccessTokenResponse.postValue(it) }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
     }
 }
