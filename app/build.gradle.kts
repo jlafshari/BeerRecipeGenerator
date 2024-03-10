5b1f67ec569e51cf341295de525cc7174be98a4d
Jahan Afshari <jahan.afshari@wwt.com>
2024-03-09 17:38:59 -0800

    remove volley dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,6 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.11.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.21")
-    implementation("com.android.volley:volley:1.2.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
