1101cede70a0346a5a7e73f075848b34c0e01c45
Jahan Afshari <jahan.afshari@wwt.com>
2024-03-09 17:49:57 -0800

    remove unused class

--- app/src/main/java/com/jlafshari/beerrecipegenerator/ui/login/User.kt
+++
@@ -1,7 +0,0 @@
-package com.jlafshari.beerrecipegenerator.ui.login
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonProperty
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class User(val name: String, @JsonProperty("sub") val id: String)
