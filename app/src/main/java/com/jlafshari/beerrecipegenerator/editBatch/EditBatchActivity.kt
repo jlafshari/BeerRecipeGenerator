64c4f2d31e8cbd0f213287650317cc7a939fa157
Jahan Afshari <jahan.afshari@wwt.com>
2024-10-31 17:23:28 -0700

    load batch and display recipe name

--- app/src/main/java/com/jlafshari/beerrecipegenerator/editBatch/EditBatchActivity.kt
+++ app/src/main/java/com/jlafshari/beerrecipegenerator/editBatch/EditBatchActivity.kt
@@ -2,16 +2,46 @@ package com.jlafshari.beerrecipegenerator.editBatch
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import com.jlafshari.beerrecipecore.batches.Batch
+import com.jlafshari.beerrecipegenerator.Constants
+import com.jlafshari.beerrecipegenerator.batches.BatchViewModel
 import com.jlafshari.beerrecipegenerator.databinding.ActivityEditBatchBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class EditBatchActivity : ComponentActivity() {
+
+    private lateinit var mBatch: Batch
+
+    private lateinit var binding: ActivityEditBatchBinding
+    private val batchViewModel: BatchViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityEditBatchBinding.inflate(layoutInflater)
+        binding = ActivityEditBatchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.txtRecipeName.text = "Test Recipe"
+        val batchId = intent.getStringExtra(Constants.EXTRA_EDIT_BATCH)!!
+        batchViewModel.loadBatchDetails(batchId)
+
+        batchViewModel.loadBatchDetailsResponse.observe(this@EditBatchActivity) {
+            if (it != null) {
+                loadBatch(it)
+                batchViewModel.loadBatchDetailsComplete()
+            }
+        }
+    }
+
+    private fun loadBatch(batch: Batch) {
+        with(batch) {
+            mBatch = this
+        }
+
+        loadBatchView()
+    }
+
+    private fun loadBatchView() {
+        binding.txtRecipeName.text = mBatch.recipe.name
     }
 }
