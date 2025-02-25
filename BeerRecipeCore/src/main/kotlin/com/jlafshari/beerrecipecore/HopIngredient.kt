f8af90fa6e3aa5e2d9fcadb59daa0dfa9758ecf0
Jahan Afshari <jahan.afshari@wwt.com>
2025-02-24 16:28:35 -0800

    display hop addition values based on hop use

--- BeerRecipeCore/src/main/kotlin/com/jlafshari/beerrecipecore/HopIngredient.kt
+++ BeerRecipeCore/src/main/kotlin/com/jlafshari/beerrecipecore/HopIngredient.kt
@@ -5,7 +5,7 @@ data class HopIngredient(val name: String,
                          var amount: Double,
                          var boilAdditionTime: Int?,
                          val hopId: String,
-                         val use: HopUse,
+                         var use: HopUse,
                          val dryHopDayStart: Int?,
                          val dryHopDayEnd: Int?,
                          val whirlpoolDuration: Int?)
--- app/src/main/java/com/jlafshari/beerrecipegenerator/editRecipe/HopEditListAdapter.kt
+++ app/src/main/java/com/jlafshari/beerrecipegenerator/editRecipe/HopEditListAdapter.kt
@@ -5,9 +5,11 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -32,17 +34,6 @@ class HopEditListAdapter(private val hopList: List<HopIngredient>,
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val hop = hopList[holder.adapterPosition]
         holder.txtHopAmount.text.insert(0, hop.amount.toString())
-        holder.txtHopAdditionTime.text.insert(0, hop.boilAdditionTime.toString())
-        holder.txtHopAdditionTime.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(hopBoilAdditionTimeEditText: Editable?) {
-                val boilAdditionTime = hopBoilAdditionTimeEditText.toString().toIntOrNull()
-                if (boilAdditionTime != null)
-                    hopBoilAdditionTimeChangedListener(boilAdditionTime, holder.adapterPosition)
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-        })
         holder.txtHop.text = hop.name
         holder.txtHopAmount.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(hopAmountEditText: Editable?) {
@@ -60,15 +51,77 @@ class HopEditListAdapter(private val hopList: List<HopIngredient>,
         holder.hopUseSpinner.adapter = ArrayAdapter(holder.itemView.context,
             android.R.layout.simple_spinner_dropdown_item, hopUses)
         holder.hopUseSpinner.setSelection(HopUse.entries.indexOf(hop.use))
+        holder.hopUseSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                hop.use = HopUse.entries[position]
+                setElementsForHopUse(hop, holder)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+        holder.txtHopBoilAdditionTime.text.clear()
+        holder.txtHopBoilAdditionTime.text.insert(0, hop.boilAdditionTime.toString())
+        holder.txtHopBoilAdditionTime.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(hopBoilAdditionTimeEditText: Editable?) {
+                val boilAdditionTime = hopBoilAdditionTimeEditText.toString().toIntOrNull()
+                if (boilAdditionTime != null)
+                    hopBoilAdditionTimeChangedListener(boilAdditionTime, holder.adapterPosition)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        holder.txtWhirlpoolAdditionTime.text.clear()
+        holder.txtWhirlpoolAdditionTime.text.insert(0, hop.whirlpoolDuration.toString())
+
+        holder.txtDryHopDayStartEdit.text.clear()
+        holder.txtDryHopDayStartEdit.text.insert(0, hop.dryHopDayStart.toString())
+        holder.txtDryHopDayEndEdit.text.clear()
+        holder.txtDryHopDayEndEdit.text.insert(0, hop.dryHopDayEnd.toString())
+
+        setElementsForHopUse(hop, holder)
     }
 
     override fun getItemCount() = hopList.size
 
+    private fun setElementsForHopUse(hop: HopIngredient, holder: ViewHolder) {
+        with(holder) {
+            when (hop.use) {
+                HopUse.Boil -> {
+                    dryHopLayout.visibility = View.GONE
+                    txtHopBoilAdditionTime.visibility = View.VISIBLE
+                    txtWhirlpoolAdditionTime.visibility = View.GONE
+                    txtHopAdditionTimeUnit.visibility = View.VISIBLE
+                }
+                HopUse.Whirlpool -> {
+                    dryHopLayout.visibility = View.GONE
+                    txtHopBoilAdditionTime.visibility = View.GONE
+                    txtWhirlpoolAdditionTime.visibility = View.VISIBLE
+                    txtHopAdditionTimeUnit.visibility = View.VISIBLE
+                }
+                else -> {
+                    dryHopLayout.visibility = View.VISIBLE
+                    txtHopBoilAdditionTime.visibility = View.GONE
+                    txtWhirlpoolAdditionTime.visibility = View.GONE
+                    txtHopAdditionTimeUnit.visibility = View.GONE
+                }
+            }
+        }
+    }
+
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val txtHopAmount: EditText = itemView.findViewById(R.id.txtHopAmountEdit)
         val txtHop: TextView = itemView.findViewById(R.id.txtHopEdit)
-        val txtHopAdditionTime: EditText = itemView.findViewById(R.id.txtHopAdditionTimeEdit)
+        val txtHopBoilAdditionTime: EditText = itemView.findViewById(R.id.txtHopBoilAdditionTimeEdit)
+        val txtWhirlpoolAdditionTime: EditText = itemView.findViewById(R.id.txtWhirlpoolAdditionTimeEdit)
+        val txtHopAdditionTimeUnit: TextView = itemView.findViewById(R.id.txtHopAdditionTimeUnit)
         val btnDeleteHop: ImageButton = itemView.findViewById(R.id.btnDeleteHop)
         val hopUseSpinner: Spinner = itemView.findViewById(R.id.hopUseSpinner)
+
+        val dryHopLayout: LinearLayout = itemView.findViewById(R.id.dryHopLayout)
+        val txtDryHopDayStartEdit: EditText = itemView.findViewById(R.id.txtDryHopDayStartEdit)
+        val txtDryHopDayEndEdit: EditText = itemView.findViewById(R.id.txtDryHopDayEndEdit)
     }
 }
